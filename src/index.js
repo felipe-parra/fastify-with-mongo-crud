fc990c71422ee980a2608614db2430d1e37fdd33
felipe parra <felipe.parra@magnotechnology.com>
2021-09-29 14:38:19 -0500

    install fastify-env

--- src/index.js
+++ src/index.js
@@ -2,13 +2,63 @@ const fastify = require("fastify")({
   logger: true,
 });
 
+const fastifyEnv = require("fastify-env");
+
+const schema = {
+  type: "object",
+  required: ["PORT", "DB_MONGO_URI"],
+  properties: {
+    PORT: {
+      type: "string",
+    },
+    DB_MONGO_URI: {
+      type: "string",
+    },
+  },
+};
+
+// const options = {
+//   confKey: "config",
+//   schema,
+//   dotenv: true,
+//   data: process.env,
+// };
+
+// const initialize = async () => {
+//   fastify.register(fastifyEnv, options);
+//   await fastify.after();
+
+//   // Database
+//   // Connection URL
+//   const username = encodeURIComponent(fastify.config.DB_USERNAME);
+//   const password = encodeURIComponent(fastify.config.DB_PASSWORD);
+//   const dbName = "databaseName";
+
+//   const url = `mongodb://${username}:${password}@localhost:27017/${dbName}`;
+
+//   fastify.register(require("./database-connector"), {
+//     url,
+//     useUnifiedTopology: true,
+//   });
+// };
+
+// initialize();
+
 fastify.get("/", (req, res) => {
   res.send({ msg: "Hello World!" });
 });
-
+const initialize = async () => {
+  fastify.register(fastifyEnv);
+};
 const start = async () => {
-  await fastify.listen(3500);
-  fastify.log.info(`Server listening on: ${fastify.server.address().port}`);
+  try {
+    await fastify.ready();
+    await fastify.listen(3500);
+    fastify.log.info(`Server listening on: ${fastify.server.address().port}`);
+  } catch (error) {
+    fastify.log.error(error);
+    process.exit();
+  }
 };
 
 start();
